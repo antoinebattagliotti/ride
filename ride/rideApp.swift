c4e65d1f6d84245f7a00f9d8fa91f339a1572ff0
Antoine BATTAGLIOTTI <antoinebattagliotti@outlook.com>
2024-09-21 11:15:31 +0200

    Initial Commit

---
+++ ride/rideApp.swift
@@ -0,0 +1,17 @@
+//
+//  rideApp.swift
+//  ride
+//
+//  Created by Antoine Battagliotti on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct rideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rideTests/rideTests.swift
@@ -0,0 +1,17 @@
+//
+//  rideTests.swift
+//  rideTests
+//
+//  Created by Antoine Battagliotti on 21/09/2024.
+//
+
+import Testing
+@testable import ride
+
+struct rideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
